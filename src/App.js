cffd2d06b126405f091f44aec9f2c9e489b51a9d
david11yf29 <david11yf29@gmail.com>
2021-02-01 18:16:26 +0800

    updated

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Hi there</div>;
+  return (
+    <div className="App">
+      {/* Sidebar */}
+
+      {/* Homepage */}
+      <section />
+
+      {/* About Me */}
+      <section />
+
+      {/* Project*/}
+      <section />
+
+      {/* My Blog */}
+      <section />
+
+      {/* Photos of me */}
+      <section />
+
+      {/* Contact */}
+      <section />
+    </div>
+  );
 }
 
 export default App;
