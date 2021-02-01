b0f302e987bf89084c119565a85101f939bb4336
david11yf29 <david11yf29@gmail.com>
2021-02-02 00:42:26 +0800

    updated

--- src/Sidebar/Sidebar.js
+++ src/Sidebar/Sidebar.js
@@ -1,11 +1,18 @@
 import React from "react";
+import profilePic from "../img/profile.png";
 import "./Sidebar.css";
+
+import GitHubIcon from "@material-ui/icons/GitHub";
+import LinkedInIcon from "@material-ui/icons/LinkedIn";
+import InstagramIcon from "@material-ui/icons/Instagram";
+import FacebookIcon from "@material-ui/icons/Facebook";
 
 const Sidebar = () => {
   return (
     <div className="sidebar">
       <div className="sidebar__header">
-        <img src="./../image/IMG_3675.JPG" alt="profile" />
+        <img src={profilePic} alt="profile" />
+        <p>David Lin</p>
       </div>
       <div className="sidebar__content">
         <p>Home</p>
@@ -16,7 +23,14 @@ const Sidebar = () => {
         <p>Contact</p>
       </div>
       <div className="sidebar__footer">
-        <h2>Footer Here</h2>
+        <h2>Copyright © 2019 built by David Lin</h2>
+        <p>david11yf29@gmail.com</p>
+        <div className="sidebar__footer__icon">
+          <GitHubIcon />
+          <LinkedInIcon />
+          <InstagramIcon />
+          <FacebookIcon />
+        </div>
       </div>
     </div>
   );
