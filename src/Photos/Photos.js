97f368855ca1c4c225782766bd3ba890f8f9c913
david11yf29 <david11yf29@gmail.com>
2021-02-06 20:59:41 +0800

    updated

--- src/Photos/Photos.js
+++ src/Photos/Photos.js
@@ -6,7 +6,7 @@ import axios from "axios";
 import Header from "../utilities/Header/Header";
 
 const instagramAPI =
-  "https://graph.instagram.com/v1.0/17841400029139525/media?access_token=IGQVJXNlBsZA1dINXB5ZAmlneWxlc0JHNGV1eVdVaFpPSXlPMlRVbnNlS0kwUDFreExSYjhQWi1hdUxFaFFvQ2wxZAkVpMFlMSWZAQNVNuYmdrTUtublFoeG5KVGp2c2NERXRwU2k5QnpMU1docUoxR1FhZA0F2Mjl4Q3VhdE5F&fields=id%2Ccaption&limit=25&after=QVFIUkhmbEZAjMjZA0dXFrY2hTTVRzUUtrQXROT2pSTjlBS0FCLWJXTTBUNXVNT2tQSllKNFZARRDlpdFNSZAFp6U3NsS2haSHh4cDJDMlV2bEItY1IwNnZAacmFn";
+  "https://graph.instagram.com/me/media?fields=id,caption,media_type,media_url&access_token=IGQVJYdmFOVnE2T09CWkpGOVliRG1JaV9QQWJCSXYxOUdwVmJlQWNhT0xmMklZAMUhXVDN0YkUwZAnZATWjRRa3pidVppbTUtZAWhQeXJWV3d5WGpReXVMUTBDX0dhWHFyUVE4OHdycUUwemQxR2xJNkpJeHFjaXdPb1YyU05v";
 
 const Photos = () => {
   const [data, setData] = useState(null);
@@ -29,7 +29,6 @@ const Photos = () => {
           return null;
         })}
       </div>
-      <div>This is my instagram Photos</div>
     </div>
   );
 };
@@ -76,7 +75,21 @@ export default Photos;
 
 // code: AQDLuL-WUvjBmuvdmHY6f_M7PE_zN9wTLu8WQ5Q2dqjWTnem3uKUffAz4Pr0lBzcdQbv0ilnurh2PqvMxhu_bR32c6AUWzaKvaQH5wk2FSCY0DDVnjQ3fn-l-awBXcI0GUp0okJQmF0HmRLUydfngRCnbG5PSv62WswWefMVUyVAEZlyd0hHOzl40lIR8LxwByc8Ah5qA19b1737sVSYff7_umylwiHXdT73cg150hMYxw
 
-// {
-//   "access_token": "IGQVJWTmNwV2w1UGYtYlNPaGUxOWJFdDcyRXhjRHk5cjFYV203RmpMM2ZAYVEpDUFg4SWhQOUVCamx2V2xwSlRWYXMtSlI4LWJ3eG1Db1BjQnJOZAjBlRlp4bVRkMjZANZAzNoczdmM0huZAVlKcmtkQldPbG1pWGFuY29FMFln",
-//   "user_id": 17841400029139525
-// }
+{
+  /* curl -X POST \
+  https://api.instagram.com/oauth/access_token \
+  -F client_id=760217521278386 \
+  -F client_secret=cbdceffc7fcb3f77ef93c9eaef06be41 \
+  -F grant_type=authorization_code \
+  -F redirect_uri=https://personal-website-db7c8.web.app/photos \
+  -F code=AQDLuL-WUvjBmuvdmHY6f_M7PE_zN9wTLu8WQ5Q2dqjWTnem3uKUffAz4Pr0lBzcdQbv0ilnurh2PqvMxhu_bR32c6AUWzaKvaQH5wk2FSCY0DDVnjQ3fn-l-awBXcI0GUp0okJQmF0HmRLUydfngRCnbG5PSv62WswWefMVUyVAEZlyd0hHOzl40lIR8LxwByc8Ah5qA19b1737sVSYff7_umylwiHXdT73cg150hMYxw */
+}
+
+{
+  /* "access_token": "IGQVJYdmFOVnE2T09CWkpGOVliRG1JaV9QQWJCSXYxOUdwVmJlQWNhT0xmMklZAMUhXVDN0YkUwZAnZATWjRRa3pidVppbTUtZAWhQeXJWV3d5WGpReXVMUTBDX0dhWHFyUVE4OHdycUUwemQxR2xJNkpJeHFjaXdPb1YyU05v",
+    "user_id": 17841400029139525 */
+}
+
+{
+  /* https://graph.instagram.com/me/media?fields=id,caption,media_type,media_url&access_token=IGQVJYdmFOVnE2T09CWkpGOVliRG1JaV9QQWJCSXYxOUdwVmJlQWNhT0xmMklZAMUhXVDN0YkUwZAnZATWjRRa3pidVppbTUtZAWhQeXJWV3d5WGpReXVMUTBDX0dhWHFyUVE4OHdycUUwemQxR2xJNkpJeHFjaXdPb1YyU05v */
+}
