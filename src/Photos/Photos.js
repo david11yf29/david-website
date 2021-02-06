4bc56a3e4304e1048f5deaa24f715c2ac4b430dc
david11yf29 <david11yf29@gmail.com>
2021-02-06 17:06:34 +0800

    updated

--- src/Photos/Photos.js
+++ src/Photos/Photos.js
@@ -5,10 +5,31 @@ import axios from "axios";
 
 import Header from "../utilities/Header/Header";
 
-const instagramAPI =
-  "https://graph.instagram.com/me/media?fields=id,caption,media_type,media_url,timestamp&access_token=IGQVJWdWNFajZAQYlN5eUpqLU9oNWdMQ1ltdnotZAkFVNnNmR2gzRjI2Vkl5VzNlU2lLRzlIa2ZAoZAERjWHhlVWZARclJwZAXItOHZAtSUV5Mk9fNllQcHVmYldqc0Njc3BpQXZAuQThXR0hsM2NyU0hyV2FkRAZDZD";
-
 const Photos = () => {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    console.log("Hello");
+  }, []);
+
+  return (
+    <div className="photos">
+      <Header title="Photos" />
+      <div className="photos__container"></div>
+      <div>This is my instagram Photos</div>
+    </div>
+  );
+};
+
+export default Photos;
+
+{
+  /* const instagramAPI =
+  "https://graph.instagram.com/me/media?fields=id,caption,media_type,media_url,timestamp&access_token=IGQVJWdWNFajZAQYlN5eUpqLU9oNWdMQ1ltdnotZAkFVNnNmR2gzRjI2Vkl5VzNlU2lLRzlIa2ZAoZAERjWHhlVWZARclJwZAXItOHZAtSUV5Mk9fNllQcHVmYldqc0Njc3BpQXZAuQThXR0hsM2NyU0hyV2FkRAZDZD"; */
+}
+
+{
+  /* const Photos = () => {
   const [data, setData] = useState(null);
   useEffect(() => {
     const fetchData = async () => {
@@ -32,29 +53,5 @@ const Photos = () => {
       <div>This is my instagram Photos</div>
     </div>
   );
-};
-
-export default Photos;
-
-// Instagram App ID = "760217521278386"
-
-// https://api.instagram.com/oauth/authorize
-//   ?client_id=760217521278386
-//   &redirect_uri=https://personal-website-db7c8.web.app/photos
-//   &scope=user_profile,user_media
-//   &response_type=code
-
-// https://personal-website-db7c8.web.app/photos?code=AQBkfYA4nQXcFiYmmrDbn66mdpcGPYzt3sICNhDbU__9OE0Wugu9tQRf5pn9jbvDROe8S73Xrwwu2uahxWXdJFk7yShRwgFgTvIWg8UPP5UAGjpHXbRO2lg85_E_qqD9_E1QxdVosSzUv5xRAHCQI6qc5vihnIf3M2f0cpyQPB3hOuUiuGSh0qmxpNah06NimrbxgnFSuhbSAAwNiNFWAnUffLGRVhZWMJJKD8x0-Dd6aA#_
-
-// curl -X POST \
-//   https://api.instagram.com/oauth/access_token \
-//   -F client_id=760217521278386 \
-//   -F client_secret=cbdceffc7fcb3f77ef93c9eaef06be41 \
-//   -F grant_type=authorization_code \
-//   -F redirect_uri=https://personal-website-db7c8.web.app/photos \
-//   -F code=AQBkfYA4nQXcFiYmmrDbn66mdpcGPYzt3sICNhDbU__9OE0Wugu9tQRf5pn9jbvDROe8S73Xrwwu2uahxWXdJFk7yShRwgFgTvIWg8UPP5UAGjpHXbRO2lg85_E_qqD9_E1QxdVosSzUv5xRAHCQI6qc5vihnIf3M2f0cpyQPB3hOuUiuGSh0qmxpNah06NimrbxgnFSuhbSAAwNiNFWAnUffLGRVhZWMJJKD8x0-Dd6aA
-
-// {
-//   "access_token": "IGQVJWaWRmWGhhRGNFUVNDeHltZA0huOXFjZAVA0cEhWN1VES01KaTdiV29DZAXhYcVJCUnNLSFB0YUdDOUFjaDhEOWVGd2xaRFp1MGZAkVmxaeG56bGphaHo1MU5SOXUzR2pGMDkyS01RQUxPdFNIdlJySndEdVR6ZAUNIZAHhj",
-//   "user_id": 17841400029139525
-// }
+}; */
+}
