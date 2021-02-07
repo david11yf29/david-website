881b388e79e94f5ce0dbd7fba0251b8b3a079968
david11yf29 <david11yf29@gmail.com>
2021-02-07 22:29:08 +0800

    updated

--- src/Skills/Skills.js
+++ src/Skills/Skills.js
@@ -4,10 +4,23 @@ import "./Skills.css";
 import Header from "../utilities/Header/Header";
 import ProgressBar from "../utilities/ProgressBar/ProgressBar";
 
-const testData = [
-  { bgcolor: "#CA8F42", completed: 60 },
-  { bgcolor: "#00695c", completed: 30 },
-  { bgcolor: "#ef6c00", completed: 53 },
+const BarData = [
+  { html: "HTML", bgcolor: "#BEB2A7", completed: 90 },
+  { css: "CSS", bgcolor: "#D1C6BF", completed: 80 },
+  { js: "JavaScript", bgcolor: "#DBD7CC", completed: 70 },
+  { react: "React", bgcolor: "#CAB388", completed: 70 },
+  { redux: "Redux", bgcolor: "#D5C4A1", completed: 60 },
+  { typescript: "Typescript", bgcolor: "#E0D4BB", completed: 40 },
+  { materialUi: "Material-UI", bgcolor: "#B5B292", completed: 20 },
+  { nodejs: "Node.js", bgcolor: "#B5B292", completed: 50 },
+  { expressjs: "Express.js", bgcolor: "#E0D4BB", completed: 40 },
+  { postgresql: "PostgreSQL", bgcolor: "#D5C4A1", completed: 40 },
+  { git: "GIT", bgcolor: "#BEB2A7", completed: 70 },
+  { vscode: "VS CODE", bgcolor: "#D1C6BF", completed: 70 },
+  { npm: "npm", bgcolor: "#DBD7CC", completed: 60 },
+  { webpack: "Webpack", bgcolor: "#CAB388", completed: 50 },
+  { firebase: "Firebase", bgcolor: "#D5C4A1", completed: 50 },
+  { heroku: "Heroku", bgcolor: "#E0D4BB", completed: 30 },
 ];
 
 const Skills = () => {
@@ -19,49 +32,53 @@ const Skills = () => {
         <div className="skills__container__content">
           <h2>Front-End</h2>
           <div className="skills__container__content__bar">
-            <p>HTML</p>
-            <ProgressBar className="bar" bgcolor="#00695c" completed="70" />
-          </div>
-          <div className="skills__container__content__bar">
-            <p>CSS</p>
+            <p>{BarData[0].html}</p>
             <ProgressBar
-              bgcolor={testData[0].bgcolor}
-              completed={testData[1].completed}
+              className="bar"
+              bgcolor={BarData[0].bgcolor}
+              completed={BarData[0].completed}
             />
           </div>
           <div className="skills__container__content__bar">
-            <p>JavaScript</p>
+            <p>{BarData[1].css}</p>
             <ProgressBar
-              bgcolor={testData[0].bgcolor}
-              completed={testData[1].completed}
+              bgcolor={BarData[1].bgcolor}
+              completed={BarData[1].completed}
             />
           </div>
           <div className="skills__container__content__bar">
-            <p>React</p>
+            <p>{BarData[2].js}</p>
             <ProgressBar
-              bgcolor={testData[0].bgcolor}
-              completed={testData[1].completed}
+              bgcolor={BarData[2].bgcolor}
+              completed={BarData[2].completed}
             />
           </div>
           <div className="skills__container__content__bar">
-            <p>Redux</p>
+            <p>{BarData[3].react}</p>
             <ProgressBar
-              bgcolor={testData[0].bgcolor}
-              completed={testData[1].completed}
+              bgcolor={BarData[3].bgcolor}
+              completed={BarData[3].completed}
             />
           </div>
           <div className="skills__container__content__bar">
-            <p>Typescript</p>
+            <p>{BarData[4].redux}</p>
             <ProgressBar
-              bgcolor={testData[0].bgcolor}
-              completed={testData[1].completed}
+              bgcolor={BarData[4].bgcolor}
+              completed={BarData[4].completed}
             />
           </div>
           <div className="skills__container__content__bar">
-            <p>Material-UI</p>
+            <p>{BarData[5].typescript}</p>
             <ProgressBar
-              bgcolor={testData[0].bgcolor}
-              completed={testData[1].completed}
+              bgcolor={BarData[5].bgcolor}
+              completed={BarData[5].completed}
+            />
+          </div>
+          <div className="skills__container__content__bar">
+            <p>{BarData[6].materialUi}</p>
+            <ProgressBar
+              bgcolor={BarData[6].bgcolor}
+              completed={BarData[6].completed}
             />
           </div>
         </div>
@@ -69,24 +86,24 @@ const Skills = () => {
         <div className="skills__container__content">
           <h2>Back-End</h2>
           <div className="skills__container__content__bar">
-            <p>Node.js</p>
+            <p>{BarData[7].nodejs}</p>
             <ProgressBar
-              bgcolor={testData[0].bgcolor}
-              completed={testData[1].completed}
+              bgcolor={BarData[7].bgcolor}
+              completed={BarData[7].completed}
             />
           </div>
           <div className="skills__container__content__bar">
-            <p>Express.js</p>
+            <p>{BarData[8].expressjs}</p>
             <ProgressBar
-              bgcolor={testData[0].bgcolor}
-              completed={testData[1].completed}
+              bgcolor={BarData[8].bgcolor}
+              completed={BarData[8].completed}
             />
           </div>
           <div className="skills__container__content__bar">
-            <p>PostgreSQL</p>
+            <p>{BarData[9].postgresql}</p>
             <ProgressBar
-              bgcolor={testData[0].bgcolor}
-              completed={testData[1].completed}
+              bgcolor={BarData[9].bgcolor}
+              completed={BarData[9].completed}
             />
           </div>
         </div>
@@ -94,45 +111,45 @@ const Skills = () => {
         <div className="skills__container__content">
           <h2>Tool</h2>
           <div className="skills__container__content__bar">
-            <p>Git</p>
+            <p>{BarData[10].git}</p>
             <ProgressBar
-              bgcolor={testData[0].bgcolor}
-              completed={testData[1].completed}
+              bgcolor={BarData[10].bgcolor}
+              completed={BarData[10].completed}
             />
           </div>
           <div className="skills__container__content__bar">
-            <p>VS Code IDE</p>
+            <p>{BarData[11].vscode}</p>
             <ProgressBar
-              bgcolor={testData[0].bgcolor}
-              completed={testData[1].completed}
+              bgcolor={BarData[11].bgcolor}
+              completed={BarData[11].completed}
             />
           </div>
           <div className="skills__container__content__bar">
-            <p>npm</p>
+            <p>{BarData[12].npm}</p>
             <ProgressBar
-              bgcolor={testData[0].bgcolor}
-              completed={testData[1].completed}
+              bgcolor={BarData[12].bgcolor}
+              completed={BarData[12].completed}
             />
           </div>
           <div className="skills__container__content__bar">
-            <p>webpack</p>
+            <p>{BarData[13].webpack}</p>
             <ProgressBar
-              bgcolor={testData[0].bgcolor}
-              completed={testData[1].completed}
+              bgcolor={BarData[13].bgcolor}
+              completed={BarData[13].completed}
             />
           </div>
           <div className="skills__container__content__bar">
-            <p>firebase</p>
+            <p>{BarData[14].firebase}</p>
             <ProgressBar
-              bgcolor={testData[0].bgcolor}
-              completed={testData[1].completed}
+              bgcolor={BarData[14].bgcolor}
+              completed={BarData[14].completed}
             />
           </div>
           <div className="skills__container__content__bar">
-            <p>Heroku</p>
+            <p>{BarData[15].heroku}</p>
             <ProgressBar
-              bgcolor={testData[0].bgcolor}
-              completed={testData[1].completed}
+              bgcolor={BarData[15].bgcolor}
+              completed={BarData[15].completed}
             />
           </div>
         </div>
