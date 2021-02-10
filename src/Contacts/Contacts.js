a9a7197d516a51299b5e30cb3118b6df349abba2
david11yf29 <david11yf29@gmail.com>
2021-02-10 14:23:01 +0800

    updated

--- src/Contacts/Contacts.js
+++ src/Contacts/Contacts.js
@@ -1,6 +1,10 @@
 import React from "react";
 import "./Contacts.css";
 
+import HomeWorkIcon from "@material-ui/icons/HomeWork";
+import SmartphoneIcon from "@material-ui/icons/Smartphone";
+import EmailIcon from "@material-ui/icons/Email";
+
 import GoogleApi from "../utilities/GoogleApi/GoogleApi";
 
 import Header from "../utilities/Header/Header";
@@ -31,11 +35,15 @@ const Contacts = () => {
           <h2>HAVE A QUESTION?</h2>
           <div className="contacts__container__information__info">
             <div className="contacts__container__information__info__icon">
-              <div />
+              <HomeWorkIcon />
               <p>1359 West Adams Blvd, Los Angeles, California, USA</p>
-              <div />
+            </div>
+            <div className="contacts__container__information__info__icon">
+              <SmartphoneIcon />
               <p>+886 937 250 024</p>
-              <div />
+            </div>
+            <div className="contacts__container__information__info__icon">
+              <EmailIcon />
               <p>david11yf29@gmail.com</p>
             </div>
           </div>
